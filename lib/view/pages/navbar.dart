b7b9508d5150d0e81d03cb7dfe368448b2378cbd
OraneOrveillon <71334686+OraneOrveillon@users.noreply.github.com>
2022-09-08 16:15:07 +0200

    Line NavBar + better SleepPage UI

--- lib/view/pages/navbar.dart
+++ lib/view/pages/navbar.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../../model/navbar_index_model.dart';
 import '../components/background.dart';
+import '../size_config.dart';
 import 'alarms_page.dart';
 import 'settings_page.dart';
 import 'sleep_page.dart';
@@ -22,30 +23,53 @@ class NavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig sizeConfig = SizeConfig(context);
     return ChangeNotifierProvider(
       create: (_) => NavigationBarIndexModel(initialIndex: 1),
       child: Consumer<NavigationBarIndexModel>(
         builder: (context, indexModel, child) => Scaffold(
           extendBody: true,
-          bottomNavigationBar: BottomNavigationBar(
-            currentIndex: indexModel.index,
-            onTap: (index) => indexModel.updateIndex(newIndex: index),
-            items: [
-              BottomNavigationBarItem(
-                icon: const ImageIcon(AssetImage('assets/icons/alarm.png')),
-                label: _labels[0],
+          bottomNavigationBar: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                height: 1,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.transparent,
+                      Theme.of(context).colorScheme.surface.withOpacity(0.2),
+                      Theme.of(context).colorScheme.surface.withOpacity(0.2),
+                      Theme.of(context).colorScheme.surface.withOpacity(0.2),
+                      Colors.transparent,
+                    ],
+                  ),
+                ),
               ),
-              BottomNavigationBarItem(
-                icon: const ImageIcon(AssetImage('assets/icons/moon.png')),
-                label: _labels[1],
-              ),
-              BottomNavigationBarItem(
-                icon: const ImageIcon(AssetImage('assets/icons/stats.png')),
-                label: _labels[2],
-              ),
-              BottomNavigationBarItem(
-                icon: const ImageIcon(AssetImage('assets/icons/settings.png')),
-                label: _labels[3],
+              Padding(
+                padding: EdgeInsets.only(top: sizeConfig.blockSizeVertical),
+                child: BottomNavigationBar(
+                  currentIndex: indexModel.index,
+                  onTap: (index) => indexModel.updateIndex(newIndex: index),
+                  items: [
+                    BottomNavigationBarItem(
+                      icon: const ImageIcon(AssetImage('assets/icons/alarm.png')),
+                      label: _labels[0],
+                    ),
+                    BottomNavigationBarItem(
+                      icon: const ImageIcon(AssetImage('assets/icons/moon.png')),
+                      label: _labels[1],
+                    ),
+                    BottomNavigationBarItem(
+                      icon: const ImageIcon(AssetImage('assets/icons/stats.png')),
+                      label: _labels[2],
+                    ),
+                    BottomNavigationBarItem(
+                      icon: const ImageIcon(AssetImage('assets/icons/settings.png')),
+                      label: _labels[3],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
--- lib/view/pages/sleep_page.dart
+++ lib/view/pages/sleep_page.dart
@@ -15,42 +15,58 @@ class SleepPage extends StatelessWidget {
       create: (_) => SleepModel(),
       child: Consumer<SleepModel>(
         builder: (context, sleepModel, child) => Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Stack(
             children: [
-              StreamBuilder(
-                stream: Stream.periodic(const Duration(seconds: 1)),
-                builder: (context, snapshot) {
-                  return Text(
-                    DateFormat('hh:mm a').format(DateTime.now()),
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText1!
-                        .copyWith(fontSize: sizeConfig.blockSizeHorizontal * 15),
-                  );
-                },
-              ),
-              Text(
-                'Alarm rings in ',
-                textAlign: TextAlign.center,
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyText1!
-                    .copyWith(fontSize: sizeConfig.blockSizeHorizontal * 5),
-              ),
-              ElevatedButton(
-                onPressed: () => sleepModel.updateIsSleeping(),
-                style: ButtonStyle(
-                  minimumSize: MaterialStateProperty.resolveWith((states) => Size(
-                        sizeConfig.blockSizeHorizontal * 50,
-                        sizeConfig.blockSizeVertical * 6,
-                      )),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    StreamBuilder(
+                      stream: Stream.periodic(const Duration(seconds: 1)),
+                      builder: (context, snapshot) {
+                        return Text(
+                          DateFormat('hh:mm a').format(DateTime.now()),
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1!
+                              .copyWith(fontSize: sizeConfig.blockSizeHorizontal * 17),
+                        );
+                      },
+                    ),
+                    Text(
+                      'Alarm rings in ',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText1!
+                          .copyWith(fontSize: sizeConfig.blockSizeHorizontal * 6),
+                    ),
+                  ],
                 ),
-                child: Text(
-                  sleepModel.isSleeping ? 'Awake' : 'I am going to bed',
-                  style: TextStyle(fontSize: sizeConfig.blockSizeVertical * 2.5),
-                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () => sleepModel.updateIsSleeping(),
+                      style: ButtonStyle(
+                        minimumSize: MaterialStateProperty.resolveWith((states) => Size(
+                              sizeConfig.blockSizeHorizontal * 60,
+                              sizeConfig.blockSizeVertical * 6.5,
+                            )),
+                      ),
+                      child: Text(
+                        sleepModel.isSleeping ? 'I\'m awake' : 'I am going to bed',
+                        style: TextStyle(fontSize: sizeConfig.blockSizeVertical * 3),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: sizeConfig.blockSizeVertical * 12,
+                  )
+                ],
               ),
             ],
           ),
